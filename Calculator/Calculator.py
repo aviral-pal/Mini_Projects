789b198e627d08e5da688ebc9470107af49b3a86
Aviral Pal <aviralpal31@gmail.com>
2024-01-20 16:36:10 +0530

    added Entry

--- Calculator/Calculator.py
+++ Calculator/Calculator.py
@@ -6,4 +6,7 @@ root.title("Simple Calculator")
 root.geometry('332x550') 
 root.minsize(332,550)
 root.maxsize(332,550)
+#creating a entry to display input numbers and return value
+e=Entry(root, width=17 , borderwidth=5,font=("arial",25))
+e.grid(row=0,column=0,columnspan=3,padx=10,pady=10)
 root.mainloop()
